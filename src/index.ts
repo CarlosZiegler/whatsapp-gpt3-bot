dc9df72d17cdc5c588aa19a7035b564002407707
Carlos Ziegler <carlos.ziegler@gmail.com>
2023-01-15 15:25:03 +0100

    fix: fix changed comands

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as whatsappProvider from "./providers/whatsapp.provider";
 import * as openAIProvider from "./providers/openAI.provider";
 import * as openAIController from "./controllers/openAI.controller";
+import { envs } from "./envs";
 
 type CommandsParams = {
   client: whatsappProvider.Whatsapp;
@@ -18,15 +19,13 @@ const commands = async ({ client, message, openAI }: CommandsParams) => {
     [iaCommands.davinci3]: async () => {
       const question = message?.text?.substring(message?.text?.indexOf(" "));
       if (question) {
-        const response = await openAIController.getImageResponse(
+        const response = await openAIController.getTextResponse(
           question,
           openAI
         );
 
         client.sendText(
-          message?.from === process.env.PHONE_NUMBER
-            ? message?.to
-            : message?.from,
+          message?.from === envs.phoneNumber ? message?.to : message?.from,
           response
         );
       }
@@ -36,10 +35,11 @@ const commands = async ({ client, message, openAI }: CommandsParams) => {
         message?.text?.indexOf(" ")
       );
       if (imgDescription) {
-        const imgUrl = await openAIController.getTextResponse(
+        const imgUrl = await openAIController.getImageResponse(
           imgDescription,
           openAI
         );
+
         client.sendImage(
           message.from === process.env.PHONE_NUMBER ? message.to : message.from,
           imgUrl,
@@ -53,20 +53,25 @@ const commands = async ({ client, message, openAI }: CommandsParams) => {
   const firstWord = message?.text?.substring(0, message?.text?.indexOf(" "));
 
   if (!firstWord || !commandsMap[firstWord]) {
+    console.log(`No command received from ${message?.from}`);
     return;
   }
 
+  console.log(`Command ${firstWord} received from ${message?.from}`);
   const command = commandsMap[firstWord];
 
   command?.();
 };
 
 async function start() {
-  const client = await whatsappProvider.getClient();
-  const openAI = openAIProvider.openAI;
-  client.onAnyMessage((message) => commands({ client, message, openAI }));
+  try {
+    const client = await whatsappProvider.getClient();
+    const openAI = openAIProvider.openAI;
+    client.onAnyMessage((message) => commands({ client, message, openAI }));
+    console.log(" Bot started successfully");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-start()
-  .then(() => console.log("Bot started"))
-  .catch(console.error);
+start();
